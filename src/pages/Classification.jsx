13d47829a6d039bb2781de5af71ec7344fb74402
Hudson Santos <hudsnss@gmail.com>
2022-11-03 01:41:00 -0300

    classification background

--- src/pages/Classification.jsx
+++ src/pages/Classification.jsx
@@ -1,9 +1,22 @@
 import styles from "./Classification.module.css"
+import { Header } from "../components/Header"
 
 export function Classification() {
     return (
-        <div>
-            Classification
+        <div className={styles.Classification}>
+            <header>
+                <Header />
+            </header>
+
+            <main>
+                <div className={styles.ClassificationBackground}>
+                    <span className={styles.ClassificationTitle}>
+                        2022 Driver Standings
+                    </span>
+
+                    <img src="src/assets/points_effect.png" />
+                </div>
+            </main>
         </div>
     )
 }
