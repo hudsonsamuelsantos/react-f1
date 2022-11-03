2505dab24d8e8bf28793d9fb4c3f4ec106061b61
Hudson Santos <hudsnss@gmail.com>
2022-11-03 00:30:21 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css"
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Timeline } from "./pages/Timeline"
+import { Classification } from "./pages/Classification";
 
 function App() {
   return (
     <div className="Container">
-      <Timeline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/timeline"} />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/classification" element={<Classification />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Classification.jsx
@@ -0,0 +1,9 @@
+import styles from "./Classification.module.css"
+
+export function Classification() {
+    return (
+        <div>
+            Classification
+        </div>
+    )
+}
