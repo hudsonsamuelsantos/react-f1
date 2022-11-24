757928d2df68390a01d12d8330b6d0b760b98d22
Hudson Santos <hudsnss@gmail.com>
2022-11-24 19:55:35 -0300

    calendar card storybook

--- src/components/CalendarCard.stories.jsx
+++ src/components/CalendarCard.stories.jsx
@@ -1,15 +1,5 @@
 import { CalendarCard } from "./CalendarCard"
 
-import CalendarBahrainFlag from "../assets/bahrain_70_x_47.png"
-import CalendarSpainFlag from "../assets/spain_70_x_47.png"
-import CalendarFranceFlag from "../assets/france_70_x_47.png"
-import CalendarBelgiumFlag from "../assets/belgium_70_x_47.png"
-import CalendarItalyFlag from "../assets/italy_70_x_47.png"
-import CalendarJapanFlag from "../assets/japan_70_x_47.png"
-import CalendarEuaFlag from "../assets/eua_70_x_47.png"
-import CalendarAustriaFlag from "../assets/austria_70_x_47.png"
-import CalendarSaudiArabiaFlag from "../assets/saudi_arabia_70_x_47.png"
-
 export default {
     title: "Components/CalendarCard",
     component: CalendarCard,
@@ -18,24 +8,6 @@ export default {
             "countryNameUppercase": "DEFAULT",
             "day": "26",
             "mounthAbbreviationUppercase": "APR"
-        }
-    },
-    argTypes: {
-        countryImage: {
-            options: [
-                CalendarBahrainFlag,
-                CalendarSpainFlag,
-                CalendarFranceFlag,
-                CalendarBelgiumFlag,
-                CalendarItalyFlag,
-                CalendarJapanFlag,
-                CalendarEuaFlag,
-                CalendarAustriaFlag,
-                CalendarSaudiArabiaFlag,
-            ],
-            control: {
-                type: "radio"
-            },
         }
     }
 }
