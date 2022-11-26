9ba5621b534e85659dd27679c7a1ebedcade568c
Hudson Santos <hudsnss@gmail.com>
2022-11-25 22:15:19 -0300

    cards storybook

--- src/components/Card.stories.jsx
+++ src/components/Card.stories.jsx
@@ -1,16 +1,5 @@
 import { Card } from "./Card"
 
-import BahrainFlag from "../assets/bahrain.png"
-import SpainFlag from "../assets/spain.png"
-import FranceFlag from "../assets/france.png"
-import BelguimFlag from "../assets/belgium.png"
-import ItalyFlag from "../assets/italy.png"
-
-import MaxVerstappenMD from "../assets/max_verstappen_md.png"
-
-import SergioPerezSM from "../assets/segio_perez_sm.png"
-import CharlesLeclercSM from "../assets/charles_leclerc_sm.png"
-
 export default {
     title: "Components/Card",
     component: Card,
@@ -18,60 +7,26 @@ export default {
 
 export const withoutPilots = {
     args: {
-        title: "TESTING",
-        days: "10 - 12",
-        countryImage: BahrainFlag,
-        countryName: "Bahrain",
-        mounth: "APR",
-        description: "FORMULA 1 ARAMCO PRE-SEASON TESTING 2022",
-    },
-    argTypes: {
-        countryImage: {
-            options: [
-                BahrainFlag,
-                SpainFlag,
-                FranceFlag,
-                BelguimFlag,
-                ItalyFlag,
-            ],
-            control: {
-                type: "radio"
-            }
+        raceCard: {
+            "titleUppercase": "TESTING",
+            "days": "10 - 12",
+            "countryName": "Bahrain",
+            "mounth": "APR",
+            "descriptionUppercase": "FORMULA 1 ARAMCO PRE-SEASON TESTING 2022",
+            "isWithPilots": false
         }
     }
 }
 
 export const withPilots = {
     args: {
-        title: "TESTING",
-        days: "10 - 12",
-        countryImage: BahrainFlag,
-        countryName: "Bahrain",
-        mounth: "APR",
-        description: "FORMULA 1 ARAMCO PRE-SEASON TESTING 2022",
-        isWithPilots: true,
-        firstPilotImage: MaxVerstappenMD,
-        secondPilotImage: SergioPerezSM,
-        thirdPilotImage: CharlesLeclercSM,
-        firstPilotNameAbbreviation: "VER",
-        secondPilotNameAbbreviation: "POR",
-        thirdPilotNameAbbreviation: "LEC",
-    },
-    argTypes: {
-        isWithPilots: {
-            control: false,
-        },
-        countryImage: {
-            options: [
-                BahrainFlag,
-                SpainFlag,
-                FranceFlag,
-                BelguimFlag,
-                ItalyFlag,
-            ],
-            control: {
-                type: "radio"
-            }
+        raceCard: {
+            "titleUppercase": "ROUND 1",
+            "days": "20 - 22",
+            "countryName": "Bahrain",
+            "mounth": "MAY",
+            "descriptionUppercase": "FORMULA 1 GULF AIR BAHRAIN GRAND PRIX 2022",
+            "isWithPilots": true
         }
     }
 }
